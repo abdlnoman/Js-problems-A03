f58358451c54668e646c5917c205ce65035bcb62
Abdullah Al Noman <111286942+abdlnoman@users.noreply.github.com>
2025-08-04 00:30:45 +0600

    solved problem06

---
+++ problem06.js
@@ -0,0 +1,7 @@
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+
+console.log((startingSalary * (1.05) ** experience).toFixed(2) );
+
