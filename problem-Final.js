bf2b6a5fc7371f947c65829c0528557753a56c43
Abdullah Al Noman <111286942+abdlnoman@users.noreply.github.com>
2025-08-03 22:27:34 +0600

    solved problem01, problem02, problem03

---
+++ problem-Final.js
@@ -0,0 +1,34 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 800;
+//write your code here
+
+console.log(area / 2);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+
+if(money >= 25000){
+    console.log("Laptop");
+
+}
+else if( money >= 10000){
+    console.log("Cycle");
+}
+else{
+    console.log("Chocolate");
+}
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11 ;
+//write your code here
+
+for(let day = 1 ; day <= lastDay ; day++ ){
+    
+    if(day % 3 === 0){
+        console.log(day + " - medicine ");
+    }
+    else{
+        console.log(day + " - rest ");
+    }
+}
